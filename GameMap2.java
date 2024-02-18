741a8ba37f11596c89349946e73c11d0d0bded90
Micerey <104989213+Micerey@users.noreply.github.com>
2024-02-18 17:32:41 +0800

    Add files via upload

---
+++ GameMap2.java
@@ -0,0 +1,31 @@
+public class GameMap2 {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________________________________________________________________");
+        System.out.println("|                                          _____________________                  _____________________             |");
+        System.out.println("|                                          |                   |                  |                   |             |");
+        System.out.println("|                                          |                   |                  |                   |             |");
+        System.out.println("|                                          |     Pavilion      | <--------------> |   Amphitheatre    |             |");
+        System.out.println("|                                          |                   |                  |                   |             |");
+        System.out.println("|                                          |___________________|                  |___________________|             |");
+        System.out.println("|                                                    |                                                              |");
+        System.out.println("|                                                    |                                                              |");
+        System.out.println("|                                                    |                                                              |");
+        System.out.println("|   _____________________                  __________|___________                 _____________________             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |   ICS New Bldg.   | <--------------> |   Student Park    | <--------------> |     Cafeteria     |             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |___________________|                  |___________________|                  |___________________|             |");
+        System.out.println("|                                                    |                                      |                       |");
+        System.out.println("|                                                    |                                      |                       |");
+        System.out.println("|                                                    |                                      |                       |");
+        System.out.println("|   _____________________                  __________|__________                  __________|__________             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |   Covered Court   | <--------------> |      Clinic       | <--------------> |     Library       |             |");
+        System.out.println("|   |                   |                  |                   |                  |                   |             |");
+        System.out.println("|   |___________________|                  |___________________|                  |___________________|             |");
+        System.out.println("|                                                                                                                   |");
+        System.out.println("|___________________________________________________________________________________________________________________|");
+    }
+}
