fe25e2b74a71ff61b04d9468f03a6523dddd1ccc
Micerey <104989213+Micerey@users.noreply.github.com>
2024-02-20 09:54:20 +0800

    Update LocationItemsGenerator.java

--- LocationItemsGenerator.java
+++ LocationItemsGenerator.java
@@ -1,17 +1,11 @@
 import java.util.Random;
 
 public class LocationItemsGenerator {
-    private static final String[] ITEMS = { "Potion", "Energy", "Medicine", "Immortality", "Chuchu_1", "Chuchu_2" };
+    private static final String[] ITEMS = { "Empanada", "Sting", "Calculator", "Baseball Bat", "Building Materials"};
 
     public static String generateItem(String location) {
-        Random random = new Random();
-        // Check if an item is claimable (50% chance)
-        if (random.nextBoolean()) {
-            // Randomly select an item from the ITEMS array
-            int index = random.nextInt(ITEMS.length);
-            return ITEMS[index];
-        } else {
-            return null; // No item to claim
-        }
+        // Remove the random check to guarantee an item
+        int index = new Random().nextInt(ITEMS.length);
+        return ITEMS[index];
     }
 }
