2e1c6a2f0f2e35faa4b9a154d9c4446b5de928dd
Micerey <104989213+Micerey@users.noreply.github.com>
2024-02-21 14:07:25 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,8 +1,12 @@
+// Player.java
+import java.util.Random;
+
 public class Player {
     private int lives;
     private int progress;
     private int stamina;
     private String currentLocation;  // New field to store the current location
+    private String currentItem;  // New field to store the current item
     private Inventory inventory;
 
     public Player(int lives, int progress, int stamina, Inventory inventory) {
@@ -37,6 +41,14 @@ public class Player {
         this.currentLocation = location;
     }
 
+    public String getCurrentItem() {
+        return currentItem;
+    }
+
+    public void setCurrentItem(String item) {
+        this.currentItem = item;
+    }
+
     public Inventory getInventory() {
         return inventory;
     }
@@ -60,26 +72,44 @@ public class Player {
     public void restoreLives(int points) {
         lives += points;
     }
-    
+
     public void restoreEnergy(int points) {
         stamina += points;
         if (stamina > 100) {
             stamina = 100;
         }
     }
-    
-    public boolean hasItem(String itemName) {
-        return inventory.getItems().containsKey(itemName) && inventory.getItems().get(itemName) > 0;
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+        if (this.stamina > 100) {
+            this.stamina = 100;
+        } else if (this.stamina < 0) {
+            this.stamina = 0;
+        }
     }
     
 
+    public boolean hasItem(String itemName) {
+        return inventory.getItems().containsKey(itemName) && inventory.getItems().get(itemName) > 0;
+    }
+
+    public boolean hasEnoughWoodenPlanks(int count) {
+        return inventory.getItems().containsKey("Wooden Planks") && inventory.getItems().get("Wooden Planks") >= count;
+    }
+
+    public int getWoodenPlanksCount() {
+        return inventory.getItems().getOrDefault("Wooden Plank", 0);
+    }
+    
     @Override
     public String toString() {
         return "Player{" +
-                ", lives=" + lives +
+                "lives=" + lives +
                 ", progress=" + progress +
                 ", stamina=" + stamina +
                 ", currentLocation='" + currentLocation + '\'' +
+                ", currentItem='" + currentItem + '\'' +
                 ", inventory=" + inventory +
                 '}';
     }
