5c600daa0ba79f76f77b70c4c91cae5f6b1f718e
Nichoruson <151758336+Nichoruson@users.noreply.github.com>
2024-03-06 23:18:52 +0800

    Update introStory.java

--- introStory.java
+++ introStory.java
@@ -1,12 +1,6 @@
 public class introStory {
         public static void introduction() {
                 String[] story = {
-
-                                "             █   █ █▀▀ █   █▀▀ █▀▀█ █▀▄▀█ █▀▀  ▀▀█▀▀ █▀▀█  █▀▀▀█ █▀▀█ █▀▄▀█ █▀▀▄ ▀█▀ ▀▀█▀▀ █▀▀ ",
-                                "             █ █ █ █▀▀ █   █   █  █ █ ▀ █ █▀▀    █   █  █  ▄▄▄▀▀ █  █ █ ▀ █ █▀▀▄  █    █   ▀▀█ ",
-                                "             █▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀   ▀ ▀▀▀    ▀   ▀▀▀▀  █▄▄▄█ ▀▀▀▀ ▀   ▀ ▀▀▀  ▄█▄   █   ▀▀▀ ",
-                                " ",
-                                " ",
                                 "       Welcome to ZombITs, where the virtual world meets the undead apocalypse! In this thrilling augmented",
                                 " reality game, you'll find yourself surrounded by hordes of zombies, armed with nothing but your skills and",
                                 " a virtual arsenal.",
@@ -33,41 +27,12 @@ public class introStory {
 
         public static void displayIO() {
                 String[] Item_Objective = {
-                                "  ITEMS TO COLLECT                 ||  OBJECTIVES",
-                                "   Item                            ||    1. Navigate through six different locations.",
-                                "     Potion                        ||    2. Gather essential materials within a limited time.",
-                                "     Energy drink                  ||    3. Solve mathematical problems for survival.",
-                                "     Weapon                        ||    4. Preserve items and survival by solving math correctly.",
-                                "     Calculator                    ||    5. Collect the correct items for the final",
-                                " ",
-                                "  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++",
-                                " ",
-                                "                               █▀▄▀█ █▀▀ █▀▀ █ █ ▄▀█ █▄ █ █ █▀▀ █▀",
-                                "                               █ ▀ █ ██▄ █▄▄ █▀█ █▀█ █ ▀█ █ █▄▄ ▄█",
-                                " ",
-                                " ",
-                                " ITEMS",
-                                " Items will be randomly distributed among the locations at the beginning of the game.",
-                                "    1. Potion          -       adds health",
-                                "    2. Energy Drink    -       adds stamina",
-                                "    3. Calculator      -       can be used in solving the problems",
-                                "    4. Weapon          -       can be used against zombies",
-                                " ",
-                                " ZOMBIE ",
-                                "    1. Random zombie encounter.",
-                                "    2. You can advance to the next location, if your answer is correct.",
-                                "    3. Otherwise, your life will decrease and a chance to lose an item.",
-                                "    4. The lost item will be randomly redistributed to another location.",
-                                " ",
-                                " STAMINA ",
-                                "    1. The player's stamina will decrease in accordance to the total distance they travelled..",
-                                "    2. When stamina runs out, the player rests overnight where they are, leading to the loss of items",
-                                " ",
-                                " ITEMS RETRIEVAL",
-                                "    1. Players must retrieve four (4) items in total to fortify their base against the final onslaught",
-                                "       of zombies.",
-                                "    2. ",
-
+                                "  ITEMS TO COLLECT                     ||  OBJECTIVES",
+                                "  Code | Item                          ||    1. Navigate through six different locations.",
+                                "  0001 | Weapon & Ammunition           ||    2. Gather essential materials within a limited time.",
+                                "  0002 | Food                          ||    3. Solve mathematical problems for survival.",
+                                "  0003 | Water                         ||    4. Preserve items and survival by solving math correctly.",
+                                "  0004 | Calculator                    ||    5. Collect the correct items for the final"
                 };
 
                 int maxLength = 0;
@@ -83,5 +48,4 @@ public class introStory {
                 }
                 System.out.println(border);
         };
-
 }
